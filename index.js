6f6f04779292af1a0be1654f91803483a4093027
Daniel Kamkha <daniel.kamkha@gmail.com>
2015-06-21 20:43:22 +0300

    strip-symbols option

--- index.js
+++ index.js
@@ -4,25 +4,27 @@
 //     Play/Pause is free software distributed under the terms of the MIT license.
 
 // TODO: handle pin, unpin, rearrange
-// TODO: options: "consume" site's indicator, show on pinned tabs
-// TODO: localization: options
+// TODO: options: show on pinned tabs
 
 (function() {
   "use strict";
 
   const { viewFor } = require("sdk/view/core");
+  const simplePrefs = require("sdk/simple-prefs");
 
   const playSymbol = "▶︎";
   const pauseSymbol = "❚❚";
   const playSymbolAlt = "▶";
-  const allSymbols = [playSymbol, pauseSymbol, playSymbolAlt];
+  const stripSymbols = [playSymbol, playSymbolAlt];
 
   // We will add Play/Pause symbol to the left of the first of these that exists.
   // In order of priority: "Noise Control" indicator, standard Close button, "Tab Mix Plus" Close button
   const addBeforeAnonids = ["noise-indicator", "close-button", "tmp-close-button"];
 
-  function addPlayPauseSymbol(worker) {
-    let xulTab = viewFor(worker.tab);
+  let workers = {}; // workers cache
+
+  function addPlayPauseSymbol(sdkTab, callback) {
+    let xulTab = viewFor(sdkTab);
     let chromeDocument = xulTab.ownerDocument;
     let playPause = chromeDocument.getAnonymousElementByAttribute(xulTab, "anonid", "play-pause");
 
@@ -37,7 +39,7 @@
         if (event.button != 0 || event.detail != 1) {
           return;
         }
-        worker.port.emit("toggle");
+        callback();
         event.stopPropagation();
       }, true);
 
@@ -63,7 +65,7 @@
   function stripSymbolsFromLabel(label) {
     let tokenArray = label.value.split(" ");
     for (let idx = 0; idx < tokenArray.length; idx++) {
-      if (allSymbols.indexOf(tokenArray[idx]) == -1) {
+      if (stripSymbols.indexOf(tokenArray[idx]) == -1) {
         label.value = tokenArray.slice(idx).join(" ");
         break;
       }
@@ -77,51 +79,59 @@
     stripSymbolsFromLabel(event.target);
   }
 
-  function addTitleObserver(sdkTab) {
+  function addRemoveTitleObserver(sdkTab, shouldAdd) {
     let xulTab = viewFor(sdkTab);
     let chromeDocument = xulTab.ownerDocument;
     let tabTitle = chromeDocument.getAnonymousElementByAttribute(xulTab, "anonid", "tab-label");
 
     if (tabTitle) {
-      stripSymbolsFromLabel(tabTitle);
-      tabTitle.addEventListener("DOMAttrModified", valueModifiedHandler, true);
-    }
-  }
-
-  function removeTitleObserver(sdkTab) {
-    let xulTab = viewFor(sdkTab);
-    let chromeDocument = xulTab.ownerDocument;
-    let tabTitle = chromeDocument.getAnonymousElementByAttribute(xulTab, "anonid", "tab-label");
-
-    if (tabTitle) {
-      tabTitle.removeEventListener("DOMAttrModified", valueModifiedHandler, true);
-      tabTitle.value = sdkTab.title;
+      if (shouldAdd) {
+        stripSymbolsFromLabel(tabTitle);
+        tabTitle.addEventListener("DOMAttrModified", valueModifiedHandler, true);
+      } else {
+        tabTitle.removeEventListener("DOMAttrModified", valueModifiedHandler, true);
+        tabTitle.value = sdkTab.title;
+      }
     }
   }
 
   function startListening(worker) {
+    let tab = worker.tab;
+    let id = tab.id;
+    let playPause = null;
+
+    workers[id] = worker;
+
+    worker.on("detach", function () {
+      if (simplePrefs.prefs["strip-symbols"]) {
+        addRemoveTitleObserver(tab, false);
+      }
+      if (playPause) {
+        playPause.remove();
+      }
+      delete workers[id];
+    });
+    worker.port.on("paused", function (paused) {
+      if (playPause) {
+        playPause.innerHTML = paused ? pauseSymbol : playSymbol;
+      }
+    });
     worker.port.once("init", function () {
-      let tab = worker.tab;
-      let playPause = addPlayPauseSymbol(worker);
-
-      worker.on("detach", function () {
-        removeTitleObserver(tab);
-        if (playPause) {
-          playPause.remove();
-        }
-      });
-      worker.port.on("paused", function (paused) {
-        if (playPause) {
-          playPause.innerHTML = paused ? pauseSymbol : playSymbol;
-        }
-      });
-
-      addTitleObserver(tab);
+      playPause = addPlayPauseSymbol(tab, function() { worker.port.emit("toggle"); });
+      if (simplePrefs.prefs["strip-symbols"]) {
+        addRemoveTitleObserver(tab, true);
+      }
     });
   }
 
   exports.main = function () {
     let self = require("sdk/self");
+
+    simplePrefs.on("strip-symbols", function () {
+      for (let id in workers) {
+        addRemoveTitleObserver(workers[id].tab, simplePrefs.prefs["strip-symbols"]);
+      }
+    });
 
     require("sdk/page-mod").PageMod({
       include: "*", // Match everything
